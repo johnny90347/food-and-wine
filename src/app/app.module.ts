dd62d19fae0df6cb789f2beee23e8dd6d7c42236
johnny <>
2021-01-04 16:00:33 +0800

    ADD package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// component
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { HeaderComponent } from './components/header/header.component';
+// material 
+
+//service
+import { DataSourceService } from './services/data-source.service';
+// store
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
   ],
-  providers: [],
+  providers: [
+    DataSourceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
