bb4b51f82d2e52938a0ce9a378c0bb227d0bd6ac
johnny <>
2021-01-14 16:42:39 +0800

    ADD wine menu

--- src/app/components/wine-menu/wine-menu.component.ts
+++ src/app/components/wine-menu/wine-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WineList } from '@core/model/model';
+import { DataSourceService } from '@core/services/data-source.service';
 
 @Component({
   selector: 'app-wine-menu',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WineMenuComponent implements OnInit {
 
-  constructor() { }
+  public wineListPartOne: WineList[];//菜單一號
+  public wineListPartTwo: WineList[];//菜單二號
+
+  constructor(private dataSourceService: DataSourceService) { }
 
   ngOnInit(): void {
+    this.getFoodListOne();
+    this.getFoodListTwo();
+  }
+
+  /** 取得菜單一號 */
+  private getFoodListOne() {
+    this.wineListPartOne = this.dataSourceService.getWineListPartOne();
+  }
+  /** 取得菜單一號 */
+  private getFoodListTwo() {
+    this.wineListPartTwo = this.dataSourceService.getWineListPartTwo();
   }
 
 }
