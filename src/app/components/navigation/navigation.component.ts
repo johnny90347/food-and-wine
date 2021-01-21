b97ec493d1ab05a398d4df285cd06c0778c24b42
johnny <>
2021-01-21 16:37:06 +0800

    update

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '@core/services/app.service';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appService: AppService) { }
 
   ngOnInit(): void {
+  }
+
+  /** 觸發滾動到預定料理 */
+  public sendScrollToSpecial() {
+    this.appService.triggerScrollToSpecialMenu();
+  }
+
+  /** 觸發滾動到最新菜單 */
+  public sendScrollToFoodMenu() {
+    this.appService.triggerScrollToFoodMenu();
+  }
+
+  /** 觸發滾動到最新酒單 */
+  public sendScrollToWineMenu() {
+    this.appService.triggerScrollToWineMenu();
+  }
+
+  /** 觸發滾動到店家位置 */
+  public sendScrollToPosition() {
+    this.appService.triggerScrollToLocation();
   }
 
 }
